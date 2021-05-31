f19db5858a8a45f2dd3f8b2a86090150d014d12a
Aadiv Shah <60790497+Aadiv1999@users.noreply.github.com>
2021-05-31 17:39:59 +0530

    Add files via upload

---
+++ Harris Corner Detection/harris_detector.m
@@ -0,0 +1,94 @@
+clc
+clear
+close all;
+
+% path = "C:\Users\aadiv\Documents\Projects\Computer Vision\images\corners.jpg";
+path = "C:\Users\aadiv\Documents\Projects\Computer Vision\images\corners.png";
+path = "C:\Users\aadiv\Documents\Projects\Computer Vision\images\house.png";
+
+
+img = imread(path);
+
+[row,column,d] = size(img);
+columnFixed = 600;
+rowNew = floor(columnFixed * (row/column));
+img = imresize(img,[rowNew,columnFixed]);
+
+gray = rgb2gray(img);
+level = 0.5;
+bw = imbinarize(gray,level)*255;
+
+ixMask = [1,0,-1; 2,0,-2; 1,0,-1];
+iyMask = [1,2,1; 0,0,0; -1,-2,-1];
+
+iX = conv2(bw, ixMask,'same');
+iY = conv2(bw, iyMask,'same');
+
+iX = bwareaopen(iX,50)*255;
+iY = bwareaopen(iY,50)*255;
+
+figure;
+subplot(1,3,1)
+imshow(bw);
+title("Thresholding");
+subplot(1,3,2)
+imshow(iX);
+title("X Mask");
+subplot(1,3,3)
+imshow(iY);
+title("Y Mask");
+
+% get derivative products
+iX2 = iX .* iX;
+iY2 = iY .* iY;
+
+iXY = iX .* iY;
+
+% window 
+n = 15;
+
+[r,c] = size(gray);
+k = 0.04; % harris K
+thresh = 500000000; % harris thresh
+gFilter = fspecial('Gaussian', n, 1);
+R = 0;
+wX2 = conv2(iX2, gFilter,'same');
+wY2 = conv2(iY2, gFilter,'same');
+wXY = conv2(iXY, gFilter,'same');
+
+corners = [];
+
+for i = 1:r
+    for j=1:c
+        
+        mat = [wX2(i,j),wXY(i,j); wXY(i,j),wY2(i,j)];
+        R = det(mat) - k*trace(mat)^2;
+        if(thresh < R)
+            corners = [corners;i,j];
+        end
+    end
+end
+
+figure;
+subplot(1,2,1)
+imshow(img);
+axis on;
+title("Original Image");
+subplot(1,2,2)
+imshow(img);hold on;
+plot(corners(:,2),corners(:,1),'+','Color','r');
+title("Harris Corner Detection");
+axis on;
+
+gaussMat = [];
+for i=1:n
+    for j=1:n
+        gaussMat = [gaussMat;i, j, gFilter(i,j)];
+    end
+end
+figure;
+[x,y] = meshgrid(1:n,1:n);
+
+% visualise Gaussain Mask
+surf(x,y,gFilter);hold on;shading interp;
+
